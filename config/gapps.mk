1581e5632ac6c58e0af2ecb6799bc74a826f6113
vikassangwan1 <vikas724@gmail.com>
2018-08-02 02:20:59 +0530

    Remove some gapps packages

--- config/gapps.mk
+++ config/gapps.mk
@@ -3,6 +3,6 @@ GAPPS_FORCE_PACKAGE_OVERRIDES := true
 GAPPS_FORCE_DIALER_OVERRIDES := true
 GAPPS_FORCE_MMS_OVERRIDES := true
 GAPPS_FORCE_BROWSER_OVERRIDES := true
-GAPPS_PRODUCT_PACKAGES += GoogleContacts talkback LatinImeGoogle PrebuiltDeskClockGoogle CalculatorGoogle TagGoogle CalendarGooglePrebuilt Photos PrebuiltGmail
+GAPPS_PRODUCT_PACKAGES += GoogleContacts talkback LatinImeGoogle TagGoogle CalendarGooglePrebuilt
 
 $(call inherit-product, vendor/opengapps/build/opengapps-packages.mk)
