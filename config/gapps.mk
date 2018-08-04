837cf73745b75132dc9b5b0961f0f9474591d165
vikassangwan1 <vikas724@gmail.com>
2018-08-04 05:38:53 +0530

    Gapps modify

--- config/gapps.mk
+++ config/gapps.mk
@@ -3,6 +3,7 @@ GAPPS_FORCE_PACKAGE_OVERRIDES := true
 GAPPS_FORCE_DIALER_OVERRIDES := true
 GAPPS_FORCE_MMS_OVERRIDES := true
 GAPPS_FORCE_BROWSER_OVERRIDES := true
-GAPPS_PRODUCT_PACKAGES += GoogleContacts talkback LatinImeGoogle TagGoogle CalendarGooglePrebuilt
+GAPPS_PRODUCT_PACKAGES += GoogleContacts talkback LatinImeGoogle TagGoogle CalendarGooglePrebuilt PrebuiltGmail
+GAPPS_EXCLUDED_PACKAGES := PackageInstallerGoogle
 
 $(call inherit-product, vendor/opengapps/build/opengapps-packages.mk)
